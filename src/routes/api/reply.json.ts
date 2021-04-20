7be1202360865fe084288413f51253c0ff1ebc87
ivoreis <reis.ivo@gmail.com>
2021-04-20 09:39:53 +0100

    initial commit

---
+++ src/routes/api/reply.json.ts
@@ -0,0 +1,11 @@
+import type { RequestHandler } from '@sveltejs/kit';
+
+export const post: RequestHandler = async (req) => {
+	const { headers, ...rest } = req;
+	console.log(rest);
+	return {
+		body: {
+			message: rest
+		}
+	};
+};
